96cb43bf879dabb11fa7f63ca8ecb5698b34419a
Joan <jbondri@gmail.com>
2023-09-08 09:45:27 +0200

    Adding the header file
    Add the header file for usage in a c project

---
+++ libasm.h
@@ -0,0 +1,10 @@
+#ifndef LIBASM_H
+#define LIBASM_H
+
+int ft_strlen(const char *str);
+char *ft_strcpy(char *dst, const char *src);
+int ft_strcmp(const char *s1, const char *s2);
+ssize_t ft_write(int fd, const void *buf, size_t count);
+ssize_t ft_read(int fd, void *buf, size_t count);
+char *ft_strdup(const char *s1);
+#endif
